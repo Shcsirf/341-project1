d6f8d860a6bfb72ecb75778b38e927f3668ea54e
Shcsirf <Shcsirf@gmail.com>
2013-10-01 17:50:44 -0400

    homework1

--- sE.s
+++ sE.s
@@ -9,20 +9,20 @@ answer:
 	.asciiz "The answer is: "
 .text
 main:
-		lui $t0, 0x1001		#Load Data(Array) into t0
+		lui $t0, 0x1000		#Load Data(Array) into t0
 		lb $t1, 0($t0)		#Load X into t1
 		lb $t2, 2($t0)		#Load Z into t2
 		add $t1, $t1, $t2	#add X+Z and store in t1
 		sb $t1, 1($t0)		#store t1 as Y
 
 		addi $v0, $0, 4		#Print question Prompt
-		lui $a0, 0x1001
+		lui $a0, 0x1000
 		addi $a0, $a0, 3
 		syscall
 
 
 		addi $v0, $0, 4		#Print Answer Prompt
-		lui $a0, 0x1001
+		lui $a0, 0x1000
 		addi $a0, $a0, 17
 		syscall
 
