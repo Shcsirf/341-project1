ab8d246a5462d24e3251560b4d5c9bf63abca549
Sean Frischmann <shcsirf@gmail.com>
2013-10-11 12:22:08 -0400

    added exit feature and edited look of menu

--- project_1.s
+++ project_1.s
@@ -5,6 +5,7 @@
 #.globl median
 .globl disString
 #.globl disArray
+.globl exitProgram
 .data
 buffer:
 	.space 200
@@ -16,10 +17,12 @@ display_string_prompt:
 	.asciiz "Your answer is\n"
 		#start at spot 257, end at spot 272
 main_menu_prompt:
-	.asciiz "Enter: 1-Enter a string 2-Display String 3-Convert String\n"
+	.asciiz "Enter:\n 1-Input String 2-Display String 3-Convert String\n"
 		#start at spot 273, end at spot 331
-	.asciiz "4-Mean 5-Median 6-Display Array\n"
-		#start at spot 332, end at spot 364
+	.asciiz " 4-Mean 5-Median 6-Display Array\n 7-Exit Program\n"
+		#start at spot 331, end at spot 381
+	.asciiz "Please enter a nunmber from the given menu\n"
+		#start at spot 382, end at spot 425
 .text
 
 main:
@@ -33,7 +36,7 @@ main:
 
 		addi $v0, $0, 4			#print main_menu_prompt
 		lui $a0, 0x1000			#load data array
-		addi $a0, $a0, 332		#prompt location in the array
+		addi $a0, $a0, 331		#prompt location in the array
 		syscall
 
 		addi $v0, $0, 5			#Read Integer
@@ -43,9 +46,16 @@ main:
 
 		beq $t0, 1,string		#branch to string
 		beq $t0, 2, disString
+		beq $t0, 7, exitProgram	#branch to exit
 
-		jr $ra					#Return to top of main
-		or $0, $0, $0			#Delay Slot(branch)
+		addi $v0, $0, 4			#print main_menu_prompt
+		lui $a0, 0x1000			#load data array
+		addi $a0, $a0, 381		#prompt location in the array
+		syscall
+
+		j main					#Return to top of main
+		or $0, $0, $0			#Delay Slot (Branch)
+
 string:
 		addi $v0, $0, 4			#print prompt
 		lui $a0, 0x1000			#load data array
@@ -56,7 +66,8 @@ string:
 		addi $a1, $0, 200		#sets max string length
 		add $s0, $0, $a0		#add input_string to s0
 		syscall
-		jal main
+		j main
+		or $0, $0, $0			#Delay Slot(branch)
 
 disString:
 		addi $v0, $0, 4			#print display_string_prompt
@@ -66,3 +77,10 @@ disString:
 		addi $v0, $0, 4			#print input_string
 		add $a0, $0, $s0		#add input_string to a0
 		syscall
+		j main
+		or $0, $0, $0			#Delay Slot(branch)
+
+exitProgram:
+		addi $v0, $0, 10		#Exit command
+		syscall
+
